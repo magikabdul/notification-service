e5b5dd34eaf8b539e794a7978c843c3414e0c468
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2023-08-23 22:19:57 +0200

    configuration for package scan

--- src/main/java/cloud/cholewa/notification/NotificationServiceApplication.java
+++ src/main/java/cloud/cholewa/notification/NotificationServiceApplication.java
@@ -2,11 +2,13 @@ package cloud.cholewa.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.tools.agent.ReactorDebugAgent;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "cloud.cholewa")
 public class NotificationServiceApplication {
 
     public static void main(String[] args) {
+        ReactorDebugAgent.init();
         SpringApplication.run(NotificationServiceApplication.class, args);
     }
 
